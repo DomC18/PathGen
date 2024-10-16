00d60583bbaa9a1f1b4e5e22337d43c98a6ec644
Dom Catania <24DCatania@student.nchstx.org>
2024-10-16 18:53:33 -0500

    switched back to stacked page

---
+++ js/home.js
@@ -0,0 +1,16 @@
+windowClicked = false;
+settingsClicked = false;
+
+function onTopClick(topID, divID, check) {
+    if (!check) {
+        document.getElementById(topID).style.textDecoration = "underline";
+        document.getElementById(divID).style.display = "initial";
+    } else {
+        document.getElementById(topID).style.textDecoration = "none";
+        document.getElementById(divID).style.display = "none";
+    }
+    check = !check;
+}
+
+document.getElementById("header-window-dropdown").addEventListener("click", () => onTopClick("header-window-dropdown", "header-window", windowClicked));
+document.getElementById("header-settings-dropdown").addEventListener("click", () => onTopClick("header-settings-dropdown", "header-settings", settingsClicked));
